61fc1035964b8c4905ab99cadc8c8e1d9a37d7e1
Arthur <44236735+vakilev@users.noreply.github.com>
2022-06-13 22:12:51 +0500

    ready steady gooo

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        Cat bean4 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == bean2);
+        System.out.println(bean3 == bean4);
         System.out.println(bean.getMessage());
     }
 }
